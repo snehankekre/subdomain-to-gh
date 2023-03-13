e52a38df5e907681932079cd992c742c7558b9d9
Snehan Kekre <snehan.minerva@gmail.com>
2023-03-14 01:35:32 +0530

    Create

---
+++ streamlit_app.py
@@ -0,0 +1,30 @@
+import requests
+
+import streamlit as st
+
+
+def get_gh_url(subdomain):
+    r = requests.get(
+        f"https://share.streamlit.io/api/v1/disambiguate?subdomain={subdomain}"
+    )
+    try:
+        coords = r.json()
+        return (
+            "https://github.com/"
+            + coords["owner"]
+            + "/"
+            + coords["repository"]
+            + "/blob/"
+            + coords["branch"]
+            + "/"
+            + coords["mainModule"]
+        )
+    except:
+        return "Error: " + r.text
+
+
+subdomain = st.text_input("Streamlit Community Cloud subdomain:", placeholder="30days")
+
+if subdomain:
+    gh = get_gh_url(subdomain)
+    st.write("Main module:", gh)
